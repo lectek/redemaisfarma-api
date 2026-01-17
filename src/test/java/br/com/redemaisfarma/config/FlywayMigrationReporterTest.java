52d3d62591173df858cc79cbbce142637ac794c9
Alex Morais Silva Júnior <lektecjava@gmail.com>
2026-01-17 01:10:57 -0300

    fix: align layout assets

--- src/test/java/br/com/redemaisfarma/config/FlywayMigrationReporterTest.java
+++ src/test/java/br/com/redemaisfarma/config/FlywayMigrationReporterTest.java
@@ -7,7 +7,6 @@ import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.read.ListAppender;
-import java.util.Arrays;
 import org.flywaydb.core.Flyway;
 import org.flywaydb.core.api.MigrationInfo;
 import org.flywaydb.core.api.MigrationInfoService;
